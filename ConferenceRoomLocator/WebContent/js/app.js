6f92c6612b60e25c166dbacbf1aa40e32f583b56
Vaidehi <Vaidehi>
2017-02-23 08:24:07 +0530

    Worked on book meeting page.

--- ConferenceRoomLocator/WebContent/js/app.js
+++ ConferenceRoomLocator/WebContent/js/app.js
@@ -11,6 +11,16 @@
 		      'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
 		      'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
 		      'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+		    })
+		    // If you specify less than all of the keys, it will inherit from the
+		    // default shades
+		    .accentPalette('red', {
+		      'default': '400',
+		    	  'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+			      'hue-2': '600'// use shade 200 for default, and keep all other shades the same
+		    })
+		    .backgroundPalette('grey', {
+		      'default': '100' // use shade 200 for default, and keep all other shades the same
 		    });
 		
 		$routeProvider
