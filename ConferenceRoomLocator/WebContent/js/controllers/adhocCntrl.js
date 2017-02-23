cfda2334f68e1f824dc327b8fbb1ffc260dab790
Ankita Singh <ankita.singh9293@gmail.com>
2017-02-23 17:01:36 +0530

    Adhoc page changes

--- ConferenceRoomLocator/WebContent/js/controllers/adhocCntrl.js
+++ ConferenceRoomLocator/WebContent/js/controllers/adhocCntrl.js
@@ -1,3 +1,4 @@
 conferenceApp.controller('adhocController', ['$scope', function($scope) {
 		$scope.message = 'adhoc controller called.';
+		$scope.rooms=["Pushya","Revati","Anuradha","Rohini","Kritika","Ashwini"];
 	}]);
